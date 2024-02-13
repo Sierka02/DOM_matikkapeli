a5786f8dac3692b4ca3d506a5f36107eb266fdc8
Sierka02 <n2kasi00@students.oamk.fi>
2024-02-13 13:58:25 +0200

    projekti valmis

---
+++ javascript/functions.js
@@ -0,0 +1,47 @@
+'use strict'
+
+let rand_num1 = 0
+let rand_num2 = 0
+
+
+let correct_answer =''
+let incorrect_answer =''
+
+
+const updateAnswerCounts = () => {
+document.querySelector('#correct_answer').innerHTML = correct_answer
+document.querySelector('#incorrect_answer').innerHTML = incorrect_answer
+}
+
+const getRandomIntNumberInRange = (min,max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomIntNumberInRange(1,10)
+    rand_num2 = getRandomIntNumberInRange (1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+});
+
+document.querySelector('button').addEventListener('click',() => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
+    if (answer === correctAnswer) {
+        alert('correct');
+        correct_answer++
+
+    } else {
+        alert ('Incorrect');
+        incorrect_answer++
+    }
+
+    randomizeNumbers()
+    document.querySelector('input').value ='';
+    updateAnswerCounts();
+})
